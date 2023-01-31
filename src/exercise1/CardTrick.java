467080571dbec87e87691ee1657ab8a1f2019e19
Juliean Do <dojulie@sheridancollege.ca>
2023-01-31 12:21:57 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -66,7 +66,7 @@ public class CardTrick {
      */
     private static void printInfo() {
     
-        System.out.println("I'm done!");
+        System.out.println("I'm done");
         System.out.println();
         
         System.out.println("My name is Juliean Do, but you can call me Juliean");
